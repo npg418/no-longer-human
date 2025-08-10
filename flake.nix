f3b9fa05d29fc6667e4ec85d752032d6f77384d0
NPG418 <narupenguin919@gmail.com>
2025-08-10 23:28:13 +0900

    create project

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Project flake for No Longer Human";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt.flakeModule
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.ruff.enable = true;
+          };
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              self.formatter.${system}
+              pkgs.python313
+            ];
+          };
+        };
+    };
+}
